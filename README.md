05d6fa2cedffe47e410d450f61bc966b045cafb8
ashilhub <sclanx100@gmail.com>
2025-04-19 17:34:13 +0700

    update 19.04 navbar change

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # nongki
 
+# Update 19.04
+
+dekstop navbar menu toggler, mobile not yet
+
 # Update 18.04
 
 responsive navbar early update.
---
+++ script/script.js
@@ -0,0 +1,8 @@
+let navMenuToggle = document.getElementById("navMenuToggle");
+let navMenu = document.getElementById("navMenu");
+let hamburger = document.getElementById("hamburger");
+let profile = document.getElementById("profile");
+
+profile.addEventListener("click", () => {
+    navMenuToggle.classList.toggle("active");
+})
